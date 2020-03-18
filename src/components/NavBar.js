592a9c49363ead8e2e10c6f2aeb9cba4e4e8e68f
BenPanyanil <44314201+BenPanyanil@users.noreply.github.com>
2020-03-18 15:08:23 +0200

    navbar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../Beseatslogo.png';
-import styled from 'styled-components'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../Beseatslogo.png";
+import styled from "styled-components";
 
 export default class NavBar extends Component {
   render() {
     return (
-      <NavWrapper className='navbar navbar-expand-sm px-sm-5 pb-4 d-flex align-items-end'>
-        <Link to='/'>
-          <img src={logo} alt="store" className="navbar-brand" width="140"></img>
-        </Link>
-        <ul className="navbar-nav">
-          <li className="nav-item ml-5">
-            <Link to='/' className="nav-link">tuotteet</Link>
-          </li>
-        </ul>
-        <Link to='/cart' className='ml-auto'>
-          <span className="mr-2">
-            <i className="fas fa-shopping-bag"></i>
-          </span>
-        </Link>
+      <NavWrapper className="navbar navbar-expand-sm px-sm-5 pb-4">
+        <div className="container align-items-end">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="store"
+              className="navbar-brand"
+              width="140"
+            ></img>
+          </Link>
+          <ul className="navbar-nav">
+            <li className="nav-item ml-sm-5">
+              <Link to="/" className="nav-link">
+                tuotteet
+              </Link>
+            </li>
+          </ul>
+          <Link to="/cart" className="ml-auto">
+            <span className="mr-2">
+              <i className="fas fa-shopping-bag"></i>
+            </span>
+          </Link>
+        </div>
       </NavWrapper>
-    )
+    );
   }
 }
 
@@ -41,4 +49,4 @@ const NavWrapper = styled.nav`
       font-size: 24px;
     }
   }
-`
+`;
